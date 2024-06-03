4606da82976d010e548929a542a874f7891260d9
bananoIII <plascencia.adrian.1f@gmail.com>
2024-06-03 00:47:38 -0600

    Renderizado dinamico de las ordenes

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,39 +1,36 @@
+"use client";
+
 import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
-import { prisma } from "@/src/lib/prisma";
+import { OrderWhitProducts } from "@/src/types";
+import { revalidatePath } from "next/cache";
+import useSWR from "swr";
 
-async function getPendingOrders() {
-  const orders = await prisma.order.findMany({
-    where: {
-      status: false,
-    },
-    include: {
-      orderProducts: {
-        include: {
-          product: true,
-        },
-      },
-    },
+export default function OrderPage() {
+  const url = "/admin/orders/api";
+  const fetcher = () =>
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => data);
+  const { data, error, isLoading } = useSWR<OrderWhitProducts[]>(url, fetcher, {
+    refreshInterval: 10000,
+    revalidateOnFocus: false,
   });
+  if (isLoading) return "cargando...";
+  if (data)
+    return (
+      <>
+        <Heading>Administrar Ordenes</Heading>
 
-  return orders;
-}
-
-export default async function OrderPage() {
-  const orders = await getPendingOrders();
-  return (
-    <>
-      <Heading>Administrar Ordenes</Heading>
-
-      {orders.length ? (
-        <div className=" grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
-          {orders.map((order) => (
-            <OrderCard key={order.id} order={order} />
-          ))}
-        </div>
-      ) : (
-        <p className="text-center">No hay ningun pendiente</p>
-      )}
-    </>
-  );
+        {data.length ? (
+          <div className=" grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
+            {data.map((order) => (
+              <OrderCard key={order.id} order={order} />
+            ))}
+          </div>
+        ) : (
+          <p className="text-center">No hay ningun pendiente</p>
+        )}
+      </>
+    );
 }
